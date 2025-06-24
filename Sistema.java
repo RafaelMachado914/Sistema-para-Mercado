2b79b5850fd5c8316f41a1cbc781c4cdb6a4ff39
RafaelMachado914 <lafaellascado2004@gmail.com>
2025-06-24 02:17:04 -0300

    Att
    Ultimo update, menu de cadastro

--- Sistema.java
+++ Sistema.java
@@ -6,16 +6,19 @@ public static Scanner input = new Scanner(System.in);
 
 public static void cadastreProduto(){
 
+    String[] produtos = new String[10];
+    float[] precos = new float[10];
+
     for(int i=0;i<10;i++){
       System.out.print("Informe o " + (i+1)+ "° Produto: ");
-      String Produto = input.nextLine();
+      produtos[i] = input.nextLine();
       System.out.println("Informe o valor do produto: ");
-      float preco = Float.parseFloat(input.nextLine());
+      precos[i] = Float.parseFloat(input.nextLine());
       System.out.println("");
 }
 }
 
-public static void chamarMenu(){
+public static void chamarMenu(float[] vetX){
 
     System.out.print("Selecione o uma das opçoes: ");
     System.out.print("");
@@ -26,6 +29,29 @@ public static void chamarMenu(){
     System.out.println("5- Sair");
 
     int opcoes=Integer.parseInt(input.nextLine());    
+
+    if (opcoes==1) {
+        System.out.println("Informe o produto que deseja vender:");
+        System.out.println("");
+        System.out.println("1-" + vetX[0]);
+        System.out.println("2-" + vetX[1]);
+        System.out.println("3-" + vetX[2]);
+        System.out.println("4-" + vetX[3]);
+        System.out.println("5-" + vetX[4]);
+        System.out.println("6-" + vetX[5]);
+        System.out.println("7-" + vetX[6]);
+        System.out.println("8-" + vetX[7]);
+        System.out.println("9-" + vetX[8]);
+        System.out.println("10-" + vetX[9]);
+
+         int vender = Integer.parseInt(input.nextLine());
+         System.out.println("");
+         System.out.println("");
+
+
+
+
+    }
 }
     public static void main(String[] args) {
 
